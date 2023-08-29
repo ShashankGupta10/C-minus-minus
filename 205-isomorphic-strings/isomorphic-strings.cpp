872cd1184c0d6955cc101ff13e719dfd7a8ba958
Shashank Gupta <shashankgupta9248@gmail.com>
2023-08-29 17:31:30 +0530

    Time: 3 ms (92.25%) | Memory: 7 MB (41.18%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, char> sToT;
+        unordered_map<char, char> sToT;  // Map to store mapping from s to t
+        unordered_map<char, bool> tMapped;  // Map to keep track of characters in t that are already mapped
 
         if (s.size() != t.size()) {
             return false;
         }
 
-        unordered_set<char> tMapped; 
-
         for (int i = 0; i < s.size(); i++) {
             if (sToT.find(s[i]) != sToT.end()) {
-                if (sToT[s[i]] == t[i] && tMapped.find(t[i]) != tMapped.end()) {
+                if (sToT[s[i]] == t[i]) {
                     continue;
                 } else {
                     return false;
                 }
             } else {
-                if (tMapped.find(t[i]) != tMapped.end()) {
-                    return false;
+                if (tMapped[t[i]]) {
+                    return false;  // t[i] is already mapped to another character in s
                 }
                 sToT[s[i]] = t[i];
-                tMapped.insert(t[i]);
+                tMapped[t[i]] = true;
             }
         }
         return true;
