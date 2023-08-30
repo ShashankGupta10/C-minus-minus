b86eb1822e862de4bdd7bd4c668f032cf91f1e6d
Shashank Gupta <shashankgupta9248@gmail.com>
2023-08-30 18:29:06 +0530

    Time: 11 ms (25.99%) | Memory: 7.7 MB (61.09%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val = node->next->val; //copying the next node value
-        node->next = node->next->next; // deleting the next node
+        node->val = node->next->val; 
+        node->next = node->next->next;
     }
 };
