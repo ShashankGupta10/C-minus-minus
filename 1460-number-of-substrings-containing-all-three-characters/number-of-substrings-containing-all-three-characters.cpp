2984ad597eca067c5b9824b867cd478ac8e94bac
Shashank Gupta <shashankgupta9248@gmail.com>
2023-09-21 15:55:44 +0530

    Time: 19 ms (79.00%) | Memory: 8.9 MB (30.26%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.size();
-        vector<int> count(3, 0); // to count the occurrences of 'a', 'b', and 'c'
-        int left = 0; // left pointer of the window
+        vector<int> count(3, 0); 
+        int left = 0;
         int result = 0;
-
         for (int right = 0; right < n; right++) {
-            count[s[right] - 'a']++; // Update the count for the current character
-
-            // Check if the current window contains 'a', 'b', and 'c'
+            count[s[right] - 'a']++; 
             while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-                result += n - right; // Increment result by the number of valid substrings ending at the current 'right' position
-                count[s[left] - 'a']--; // Move the left pointer of the window
+                result += n - right; 
+                count[s[left] - 'a']--;
                 left++;
             }
         }
-
         return result;
     }
 };
