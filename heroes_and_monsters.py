83bbb1a63138c361c6c81ff798a80a7975b55da0
Shashank Gupta <shashankgupta9248@gmail.com>
2023-10-02 13:17:25 +0530

    Delete heroes_and_monsters.py

--- heroes_and_monsters.py
+++
@@ -1,4 +0,0 @@
-# There are N heroes and M monsters, the powers of heroes and monsters are reported in the array.
-# a hero can defeat a monster if his power is greater than a monster,
-# and after defeating he gets some coins. so we have to find the how many coins we can collect,
-
