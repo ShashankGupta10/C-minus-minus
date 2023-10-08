cb246fb65eef8591e5a63e6c6254960f87f8ec85
Shashank Gupta <shashankgupta9248@gmail.com>
2023-10-09 00:11:58 +0530

    Time: 5 ms (8.33%) | Memory: 6.8 MB (31.50%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.cpp
+++ 728-self-dividing-numbers/self-dividing-numbers.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int> ans;
-        for (int i=left;i<=right;i++) {
+        for (int i=left;i<=right;++i) {
             string curr = to_string(i);
             bool flag = false;
-            for (int j=0;j<curr.size();j++) {
+            for (int j=0;j<curr.size();++j) {
                 int num = curr[j]-'0';
-                if (num==0 || i%num!=0) {
-                    flag = true;
-                } 
+                if (num==0 || i%num!=0) flag = true;
             }
             if (!flag) ans.emplace_back(i);
         }
