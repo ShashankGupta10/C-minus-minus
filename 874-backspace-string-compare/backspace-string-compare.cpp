6c9c9077022b912a9f06d461e0a6423821693ce6
Shashank Gupta <shashankgupta9248@gmail.com>
2023-10-23 11:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (22.27%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        string one = "";
+        string two = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '#') {
+                if (!one.empty()) {
+                    one = one.substr(0, one.size() - 1);
+                }
+            } else {
+                one += s[i];
+            }
+        }
+        for (int i = 0; i < t.size(); i++) {
+            if (t[i] == '#') {
+                if (!two.empty()) {
+                    two = two.substr(0, two.size() - 1);
+                }
+            } else {
+                two += t[i];
+            }
+        }
+        return one == two;
+    }
+};
