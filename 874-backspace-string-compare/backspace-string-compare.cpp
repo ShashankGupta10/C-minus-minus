c4d9b600e6bf9801322d15cf284287141ac007ca
Shashank Gupta <shashankgupta9248@gmail.com>
2023-10-23 11:51:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (10.60%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -5,21 +5,15 @@ public:
         string two = "";
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '#') {
-                if (!one.empty()) {
-                    one = one.substr(0, one.size() - 1);
-                }
-            } else {
-                one += s[i];
-            }
+                if (!one.empty()) one = one.substr(0, one.size() - 1);
+            } 
+            else one += s[i];
         }
         for (int i = 0; i < t.size(); i++) {
             if (t[i] == '#') {
-                if (!two.empty()) {
-                    two = two.substr(0, two.size() - 1);
-                }
-            } else {
-                two += t[i];
+                if (!two.empty()) two = two.substr(0, two.size() - 1);
             }
+            else two += t[i];
         }
         return one == two;
     }
