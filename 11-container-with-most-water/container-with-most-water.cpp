893343aa5fffb150faf07ae42bc9727a29523554
Shashank Gupta <shashankgupta9248@gmail.com>
2023-11-12 12:21:03 +0530

    Time: 80 ms (13.35%) | Memory: 59.3 MB (86.09%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -5,13 +5,8 @@ public:
         int i=0, j=height.size()-1;
         while (i<j) {
             max_area = max((j-i)*min(height[i], height[j]), max_area);
-            if (height[i]<height[j]) {
-                i++;
-            }
-            else {
-                j--;
-            }
-
+            if (height[i]<height[j]) i++;
+            else j--;
         }
         return max_area;
     }
