1f618e7f9f7f744af3a16d19aecb7cc38f4dfe17
Shashank Gupta <shashankgupta9248@gmail.com>
2024-01-10 18:50:18 +0530

    Time: 12 ms (72.22%) | Memory: 24.7 MB (29.32%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,14 +1,14 @@
 class Solution {
     void dfs(TreeNode* root, vector<int> &ans, int k){
-        if(!root || ans.size() > k) return;
+        if(!root) return;
         dfs(root -> left, ans, k);
-        if(ans.size() < k) ans.push_back(root -> val);
+        ans.push_back(root -> val);
         dfs(root -> right, ans, k);
     }
 public:
     int kthSmallest(TreeNode* root, int k) {
         vector<int> ans;
         dfs(root, ans, k);
-        return ans.back();
+        return ans[k - 1];
     }
 };
