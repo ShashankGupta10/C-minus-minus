1ae5153cc4a2c4d5d7f3951a4a3eb40959eb3916
Shashank Gupta <shashankgupta9248@gmail.com>
2024-02-08 17:35:07 +0530

    Time: 12 ms (47.29%) | Memory: 9.4 MB (44.34%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,20 +1,23 @@
-class Solution {   
+class Solution {
 public:
-    static bool compare(const std::pair<char, int>& a, const std::pair<char, int>& b) {
-        return a.second > b.second;
-    } 
     string frequencySort(string s) {
-        unordered_map<char, int> mp;
-        for (char i : s) {
-            mp[i]++;
-        }
-        vector<pair<char, int>> freqVec(mp.begin(), mp.end());
-        sort(freqVec.begin(), freqVec.end(), compare);
-        string res = "";
-        for (const auto& it : freqVec) {
-            for (int i = 0; i < it.second; i++) {
-                res += it.first;
+        vector<pair<char, int>> v;
+        for (char ch: s) {
+            int i;
+            for (i = 0; i < v.size(); ++i) {
+                if (v[i].first == ch) {
+                    v[i].second++;
+                    break;
+                }
             }
+            if (i == v.size()) v.emplace_back(make_pair(ch, 1));
+        }
+        sort(v.begin(), v.end(), [](pair<char, int> &a, pair<char, int> &b) {
+            return a.second > b.second;
+        });
+        string res = "";
+        for (int i = 0; i < v.size(); i++) {
+            for (int j = 0; j < v[i].second; j++) res += v[i].first; 
         }
         return res;
     }
