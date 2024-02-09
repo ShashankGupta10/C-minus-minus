3a3b805e8688cb0db2eb0028e69e1cdc5ed2fd55
Shashank Gupta <shashankgupta9248@gmail.com>
2024-02-09 15:30:56 +0530

    Time: 44 ms (87.99%) | Memory: 10.5 MB (32.68%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-    int helper(vector<int>& v, int n, int size, vector<vector<int>>& dp) {
-        if (n == 0) return 0;
-        if (size < 0) return n + 1;
-        if (dp[n][size] != -1) return dp[n][size];
-        int take = INT_MAX;
-        if (v[size] <= n) {
-            take =  helper(v, n - v[size], size, dp);
-            if (take != INT_MAX) take++;
-        }
-        int notTake =  helper(v, n, size - 1, dp);
-        return dp[n][size] = min(take, notTake);
-    }
     int numSquares(int n) {
-        vector<int> v;
-        for (int i = 1; i * i <= n; ++i) v.push_back(i * i);        
-        vector<vector<int>> dp(n + 1, vector<int>(v.size(), -1));
-        return helper(v, n, v.size() - 1, dp);
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+        return dp[n];
     }
 };
